a26d9a65998996396ee6c51b3efb763784e4e197
Jaysen <jaysencarlo13@gmail.com>
2021-07-16 06:22:52 +0800

    Add user schema

---
+++ models/user.js
@@ -0,0 +1,83 @@
+const { Schema, model, Types } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+	name: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	birthdate: {
+		type: Date,
+		required: false,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	contact: {
+		type: String,
+	},
+	home: {
+		type: String,
+	},
+	work_info: {
+		employee_id: {
+			type: String,
+			unique: true,
+		},
+		employee_status: {
+			type: String,
+		},
+		department: {
+			type: String,
+		},
+		title: {
+			type: String,
+		},
+		join_date: {
+			type: Date,
+		},
+	},
+
+	user_type: {
+		is_admin: {
+			type: Boolean,
+			default: false,
+		},
+		is_manager: {
+			type: Boolean,
+			default: false,
+		},
+		is_hr: {
+			type: Boolean,
+			default: false,
+		},
+		is_employee: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	my_interview: [
+		{
+			userid: { type: Types.ObjectId },
+		},
+	],
+});
+
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) next();
+	this.password = await bcrypt.hash(this.password, 15);
+	return next();
+});
+
+userSchema.methods.checkPassword = async function (password) {
+	const result = await bcrypt.compare(password, this.password);
+	return result;
+};
+
+module.exports = model('User', userSchema);
