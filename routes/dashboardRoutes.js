1528ae2698f3c559333b98406cdae22335c45229
Jaysen <jaysencarlo13@gmail.com>
2021-07-17 13:41:12 +0800

    Add fetchdashboard and checkAuth routes

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getDashboard, getUser, getLogout, updateUser, changepassword } = require('../controllers/dashboardController');
+const { getDashboard, getUser, getLogout, updateUser, changepassword, checkAuth, getMyInterviews } = require('../controllers/dashboardController');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', authMiddleware, getDashboard);
+router.post('/checkAuth', authMiddleware, checkAuth);
 router.post('/user', authMiddleware, getUser);
 router.post('/userupdate', authMiddleware, updateUser);
 router.post('/changepassword', authMiddleware, changepassword);
+router.post('/fetchdashboard', authMiddleware, getMyInterviews);
 router.post('/logout', authMiddleware, getLogout);
 
 module.exports = router;
