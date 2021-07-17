f60827c7ec3ae227b80bea5855926620430e14e6
Jaysen <jaysencarlo13@gmail.com>
2021-07-17 13:43:37 +0800

    Add a if condition to determine if there is a value of req.body. This is to avoid empty req.body

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -3,20 +3,23 @@ const moment = require('moment');
 
 exports.postRegister = async (req, res, next) => {
 	try {
-		const { name, email, birthdate, password } = req.body;
-		const user = new User({
-			name: name,
-			email: email,
-			birthdate: moment(birthdate, 'YYYY-MM-DD'),
-			password: password,
-		});
+		if (req.body) {
+			const { name, email, birthdate, password } = req.body;
+			const user = new User({
+				name: name,
+				email: email,
+				birthdate: moment(birthdate, 'YYYY-MM-DD'),
+				password: password,
+			});
 
-		await user.save((err) => {
-			if (err && err.code === 11000) {
-				return res.status(500).json({ error: err, message: 'User already exist!' });
-			}
-			return res.json({ success: true, message: 'Registration Success!' });
-		});
+			await user.save((err) => {
+				if (err && err.code === 11000) {
+					console.log(err);
+					return res.status(500).json({ error: err, message: 'User already exist!' });
+				}
+				return res.json({ success: true, message: 'Registration Success!' });
+			});
+		}
 	} catch (err) {
 		if (err) {
 			return res.statusCode(500).json({ error: err, message: 'Something went wrong!' });
