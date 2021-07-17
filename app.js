51d460062db66d0843d7b85038439f222ef0acaa
Jaysen <jaysencarlo13@gmail.com>
2021-07-17 13:38:41 +0800

    Add insert in interviews for testing purposes only.

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const { json, urlencoded } = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const app = express();
+const Interview = require('./models/interviews');
+const moment = require('moment');
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
@@ -40,6 +42,16 @@ app.use('/api', require('./routes/dashboardRoutes'));
 app.use('/register', require('./routes/registerRoutes'));
 app.use('/login', require('./routes/loginRoutes'));
 
-app.listen(3001, (err) => {
+app.listen(3001, async (err) => {
 	if (err) throw err;
+	const user = await Interview.find({});
+	if (user.length === 0) {
+		const interview = new Interview({
+			userId: '60f1d4e0429ab43a547d8a80',
+			interviewee: '60f1d510429ab43a547d8a84',
+			assignBy: '60f1d4f9429ab43a547d8a82',
+			date_time: moment().add(3, 'days'),
+		});
+		await interview.save();
+	}
 });
