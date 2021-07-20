722542d6f06add5172fc8794a1e643caf9dfc6de
Jaysen <jaysencarlo13@gmail.com>
2021-07-20 15:15:00 +0800

    Remove trigger instead declare it in dashboard parent

--- clientserver/src/components/dashboard/MyInterviews.jsx
+++ clientserver/src/components/dashboard/MyInterviews.jsx
@@ -1,30 +1,30 @@
 import { Table } from 'react-bootstrap';
 import moment from 'moment';
 
-export default function MyInterviews({ data, trigger }) {
-	const data_ = data[trigger];
-	return (
-		<Table striped bordered hover size="sm">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Assign By</th>
-					<th>Date and Time</th>
-				</tr>
-			</thead>
-			<tbody>
-				{data_.map((currentValue, index) => {
-					return (
-						<tr key={index}>
-							<td>{currentValue.name}</td>
-							<td>{currentValue.email}</td>
-							<td>{currentValue.assignBy}</td>
-							<td>{moment(currentValue.date_time).format('MMMM DD, YYYY ')}</td>
-						</tr>
-					);
-				})}
-			</tbody>
-		</Table>
-	);
+export default function MyInterviews({ data }) {
+  return (
+    <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Assign By</th>
+          <th>Date and Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((currentValue, index) => {
+          const { name, email, assignBy, date_time } = currentValue;
+          return (
+            <tr key={index}>
+              <td>{name}</td>
+              <td>{email}</td>
+              <td>{assignBy}</td>
+              <td>{moment(date_time).format('MMMM DD, YYYY hh:mm A, dddd')}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
 }
