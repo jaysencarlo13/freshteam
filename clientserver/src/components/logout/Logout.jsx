fdc5a72dc16c6aa8794d773c9d3bc00620850fcc
Jaysen <jaysencarlo13@gmail.com>
2021-07-20 15:04:01 +0800

    Reinstate the Auth from {Auth} hooks.

--- clientserver/src/components/logout/Logout.jsx
+++ clientserver/src/components/logout/Logout.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import Auth from '../../Auth';
+import { Auth } from '../../Auth';
 
 function Logout() {
 	const [logout, setLogout] = useState(false);
