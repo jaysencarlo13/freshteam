4c992170c8071f26b951b9e7f6285cbb84b9d32c
Jaysen <jaysencarlo13@gmail.com>
2021-07-23 20:22:55 +0800

    Change only in white space

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -4,215 +4,253 @@ const Session = require('../models/session');
 const Interviews = require('../models/interviews');
 const Applicants = require('../models/applicant');
 const JobPostings = require('../models/job_postings');
+const Organization_Members = require('../models/organization_members');
 
 exports.checkAuth = (req, res, next) => {
-	if (req.body) {
-		return res.json({ isAuthenticated: true });
-	}
+    if (req.body) {
+        return res.json({ isAuthenticated: true });
+    }
 };
 
 exports.getDashboard = async (req, res, next) => {
-	res.json({ isAuthenticated: true });
+    res.json({ isAuthenticated: true });
 };
 
 exports.postDashboard = async (req, res, next) => {};
 
 exports.getUser = async (req, res, next) => {
-	try {
-		const { user } = req.body;
-		const _user = await User.findById(user._id);
-		res.json({ user: _user });
-	} catch (err) {
-		res.status(500).json({ error: err, message: 'Something went wrong' });
-	}
+    try {
+        const { user } = req.body;
+        const _user = await User.findById(user._id);
+        res.json({ user: _user });
+    } catch (err) {
+        res.status(500).json({ error: err, message: 'Something went wrong' });
+    }
 };
 
 exports.getLogout = async (req, res, next) => {
-	try {
-		const { sessionID } = req.body;
-		if (sessionID) {
-			req.logout();
-			await Session.findByIdAndDelete(sessionID);
-			res.json({ isLogout: true, message: 'Success' });
-		}
-	} catch (err) {
-		res.status(500).json({ error: err, message: 'Something went wrong!' });
-	}
+    try {
+        const { sessionID } = req.body;
+        if (sessionID) {
+            req.logout();
+            await Session.findByIdAndDelete(sessionID);
+            res.json({ isLogout: true, message: 'Success' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err, message: 'Something went wrong!' });
+    }
 };
 
 exports.updateUser = async (req, res, next) => {
-	try {
-		if (req.body) {
-			const { personal_info, work_info, user } = req.body;
-			await User.updateOne(
-				{ _id: user._id },
-				{
-					...personal_info,
-					work_info,
-				}
-			);
-			res.json({ isUpdated: true, message: 'Success updating user' });
-		}
-	} catch (err) {
-		res.status(500).json({ error: err, message: 'Something went wrong!' });
-	}
+    try {
+        if (req.body) {
+            const { personal_info, work_info, user } = req.body;
+            await User.updateOne(
+                { _id: user._id },
+                {
+                    ...personal_info,
+                    work_info,
+                }
+            );
+            res.json({ isUpdated: true, message: 'Success updating user' });
+        }
+    } catch (err) {
+        res.status(500).json({ error: err, message: 'Something went wrong!' });
+    }
 };
 
 exports.changepassword = async (req, res, next) => {
-	try {
-		if (req.body) {
-			const { password, newpassword, user } = req.body;
-			const userthis = await User.findById(user._id);
-			const isValid = await userthis.checkPassword(password);
-			if (isValid) {
-				userthis.password = newpassword;
-				await userthis.save();
-				res.json({ isChangePassword: true, message: 'Success Changing Password' });
-			} else {
-				res.json({ isChangePassword: false, message: 'Password Incorrect' });
-			}
-		}
-	} catch (err) {
-		res.status(500).json({ error: err, message: 'Something went Wrong' });
-	}
+    try {
+        if (req.body) {
+            const { password, newpassword, user } = req.body;
+            const userthis = await User.findById(user._id);
+            const isValid = await userthis.checkPassword(password);
+            if (isValid) {
+                userthis.password = newpassword;
+                await userthis.save();
+                res.json({ isChangePassword: true, message: 'Success Changing Password' });
+            } else {
+                res.json({ isChangePassword: false, message: 'Password Incorrect' });
+            }
+        }
+    } catch (err) {
+        res.status(500).json({ error: err, message: 'Something went Wrong' });
+    }
 };
 
 exports.getMyInterviews = async (req, res, next) => {
-	try {
-		if (req.body) {
-			const { user } = req.body;
-			const arrayToday = [];
-			const arrayMissed = [];
-			const arrayUpcoming = [];
-			const arrayReferrals = [];
-			const arrayBirthdaycorner = [];
-			const arrayNewJoinees = [];
+    try {
+        if (req.body) {
+            const { user } = req.body;
+            const arrayToday = [];
+            const arrayMissed = [];
+            const arrayUpcoming = [];
+            const arrayReferrals = [];
+            const arrayBirthdaycorner = [];
+            const arrayNewJoinees = [];
 
-			const users = await User.find();
-			const applicant = await Applicants.find();
-			const job_postings = await JobPostings.find();
-			const { work_info } = await User.findById(user._id);
-			const myinterviews_today = await Interviews.find({ interviewer: user._id, date_time: { $gte: moment().startOf('day').toDate(), $lte: moment().endOf('day').toDate() } });
-			const myinterviews_missed = await Interviews.find({ interviewer: user._id, date_time: { $lte: moment().startOf('day').toDate() } });
-			const myinterviews_upcoming = await Interviews.find({ interviewer: user._id, date_time: { $gte: moment().startOf('day').toDate() } });
-			const myReferrals = await Applicants.find({ 'job_applications.referred_by': user._id });
-			const birthdayCorner = await User.aggregate([
-				{
-					$match: {
-						$expr: {
-							$and: [
-								{
-									$eq: [{ $month: '$birthdate' }, { $month: moment().toDate() }],
-								},
-								{
-									$gte: [{ $dayOfMonth: '$birthdate' }, { $dayOfMonth: moment().toDate() }],
-								},
-								{
-									$lte: [{ $dayOfMonth: '$birthdate' }, { $dayOfMonth: moment().endOf('month').toDate() }],
-								},
-							],
-						},
-					},
-				},
-				{
-					$sort: {
-						birthdate: 1,
-					},
-				},
-			]);
-			const newJoinees = await User.find({ 'work_info.organization_id': work_info.organization_id, 'work_info.join_date': { $gte: moment().subtract(7, 'days').toDate(), $lte: moment().toDate() } });
+            const users = await User.find();
+            const applicant = await Applicants.find();
+            const job_postings = await JobPostings.find();
+            const { organization_id } = await Organization_Members.findOne({ member_id: user._id });
+            const myinterviews_today = await Interviews.find({
+                interviewer: user._id,
+                date_time: { $gte: moment().startOf('day').toDate(), $lte: moment().endOf('day').toDate() },
+            });
+            const myinterviews_missed = await Interviews.find({
+                interviewer: user._id,
+                date_time: { $lte: moment().startOf('day').toDate() },
+            });
+            const myinterviews_upcoming = await Interviews.find({
+                interviewer: user._id,
+                date_time: { $gte: moment().startOf('day').toDate() },
+            });
+            const myReferrals = await Applicants.find({ 'job_applications.referred_by': user._id });
+            const birthdayCorner = await User.aggregate([
+                {
+                    $match: {
+                        $expr: {
+                            $and: [
+                                {
+                                    $eq: [{ $month: '$birthdate' }, { $month: moment().toDate() }],
+                                },
+                                {
+                                    $gte: [{ $dayOfMonth: '$birthdate' }, { $dayOfMonth: moment().toDate() }],
+                                },
+                                {
+                                    $lte: [
+                                        { $dayOfMonth: '$birthdate' },
+                                        { $dayOfMonth: moment().endOf('month').toDate() },
+                                    ],
+                                },
+                            ],
+                        },
+                    },
+                },
+                {
+                    $sort: {
+                        birthdate: 1,
+                    },
+                },
+            ]);
+            const newJoinees = await User.find({
+                'work_info.organization_id': organization_id,
+                'work_info.join_date': {
+                    $gte: moment().subtract(7, 'days').toDate(),
+                    $lte: moment().toDate(),
+                },
+            });
 
-			if (myinterviews_today.length) {
-				myinterviews_today.forEach((element) => {
-					const userthis = applicant.find((e) => e._id.toString() === element.interviewee.toString());
-					const name = userthis.personal_info.name;
-					const email = userthis.personal_info.email;
-					const assignBy = users.find((e) => e._id.toString() === element.assignBy.toString()).name;
-					const date_time = element.date_time;
-					arrayToday.push({
-						name: name,
-						email: email,
-						assignBy: assignBy,
-						date_time: date_time,
-					});
-				});
-			}
-			if (myinterviews_missed.length) {
-				myinterviews_missed.forEach((element) => {
-					const userthis = applicant.find((e) => e._id.toString() === element.interviewee.toString());
-					const name = userthis.personal_info.name;
-					const email = userthis.personal_info.email;
-					const assignBy = users.find((e) => e._id === element.assignBy).name;
-					const date_time = element.date_time;
-					arrayMissed.push({
-						name: name,
-						email: email,
-						assignBy: assignBy,
-						date_time: date_time,
-					});
-				});
-			}
-			if (myinterviews_upcoming.length) {
-				myinterviews_upcoming.forEach((element) => {
-					const userthis = applicant.find((e) => e._id.toString() === element.interviewee.toString());
-					const name = userthis.personal_info.name || '';
-					const email = userthis.personal_info.email || '';
-					const assignBy = users.find((e) => e._id.toString() === element.assignBy.toString()).name;
-					const date_time = element.date_time;
-					arrayUpcoming.push({
-						name: name,
-						email: email,
-						assignBy: assignBy,
-						date_time: date_time,
-					});
-				});
-			}
-			if (myReferrals.length) {
-				myReferrals.forEach((element) => {
-					const { name, email } = element.personal_info;
-					let job_applications = [];
-					// element.job_applications.find((e) => e.referred_by.toString() === user._id.toString());
-					element.job_applications.find((e) => {
-						if (e.referred_by.toString() === user._id.toString()) {
-							job_applications.push(e);
-						}
-					});
-					job_applications.forEach(({ applied_job, date_applied, status }) => {
-						const { title } = job_postings.find((e) => e._id.toString() === applied_job.toString());
-						arrayReferrals.push({
-							name: name,
-							email: email,
-							applied_job: `${title} / ${status}`,
-							date_applied: date_applied,
-						});
-					});
-				});
-			}
-			if (birthdayCorner.length) {
-				birthdayCorner.forEach((element) => {
-					const { name, birthdate } = element;
-					arrayBirthdaycorner.push({
-						name: name,
-						birthdate: moment(birthdate).format('MMMM DD'),
-					});
-				});
-			}
-			if (newJoinees.length) {
-				newJoinees.forEach(({ name, email, work_info }) => {
-					arrayNewJoinees.push({
-						name: name,
-						email: email,
-						department: work_info.department,
-						title: work_info.title,
-						join_date: moment(work_info.join_date).format('MMMM DD, YYYY'),
-					});
-				});
-			}
-
-			res.json({ isSuccess: true, today: arrayToday, missed: arrayMissed, upcoming: arrayUpcoming, referrals: arrayReferrals, birthday_corner: arrayBirthdaycorner, new_joinees: arrayNewJoinees });
-		}
-	} catch (err) {
-		console.log(err);
-		res.status(500).json({ error: err, message: 'Something Went Wrong' });
-	}
+            if (myinterviews_today.length) {
+                myinterviews_today.forEach((element) => {
+                    const userthis = applicant.find(
+                        (e) => e._id.toString() === element.interviewee.toString()
+                    );
+                    const name = userthis.personal_info.name;
+                    const email = userthis.personal_info.email;
+                    const assignBy = users.find((e) => e._id.toString() === element.assignBy.toString()).name;
+                    const date_time = element.date_time;
+                    arrayToday.push({
+                        name: name,
+                        email: email,
+                        assignBy: assignBy,
+                        date_time: date_time,
+                    });
+                });
+            }
+            if (myinterviews_missed.length) {
+                myinterviews_missed.forEach((element) => {
+                    const userthis = applicant.find(
+                        (e) => e._id.toString() === element.interviewee.toString()
+                    );
+                    const name = userthis.personal_info.name;
+                    const email = userthis.personal_info.email;
+                    const assignBy = users.find((e) => e._id.toString() === element.assignBy.toString()).name;
+                    const date_time = element.date_time;
+                    arrayMissed.push({
+                        name: name,
+                        email: email,
+                        assignBy: assignBy,
+                        date_time: date_time,
+                    });
+                });
+            }
+            if (myinterviews_upcoming.length) {
+                myinterviews_upcoming.forEach((element) => {
+                    const userthis = applicant.find(
+                        (e) => e._id.toString() === element.interviewee.toString()
+                    );
+                    const name = userthis.personal_info.name || '';
+                    const email = userthis.personal_info.email || '';
+                    const assignBy = users.find((e) => e._id.toString() === element.assignBy.toString()).name;
+                    const date_time = element.date_time;
+                    arrayUpcoming.push({
+                        name: name,
+                        email: email,
+                        assignBy: assignBy,
+                        date_time: date_time,
+                    });
+                });
+            }
+            if (myReferrals.length) {
+                myReferrals.forEach((element) => {
+                    const { name, email } = element.personal_info;
+                    let job_applications = [];
+                    // element.job_applications.find((e) => e.referred_by.toString() === user._id.toString());
+                    element.job_applications.find((e) => {
+                        if (e.referred_by.toString() === user._id.toString()) {
+                            job_applications.push(e);
+                        }
+                    });
+                    job_applications.forEach(({ applied_job, date_applied, status }) => {
+                        const jp = job_postings.find((e) => e._id.toString() === applied_job.toString());
+                        arrayReferrals.push({
+                            name: name,
+                            email: email,
+                            applied_job: `${
+                                jp
+                                    ? jp.title
+                                        ? jp.title
+                                        : 'Job is no longer available'
+                                    : 'Job is no longer available'
+                            } / ${status}`,
+                            date_applied: date_applied,
+                        });
+                    });
+                });
+            }
+            if (birthdayCorner.length) {
+                birthdayCorner.forEach((element) => {
+                    const { name, birthdate } = element;
+                    arrayBirthdaycorner.push({
+                        name: name,
+                        birthdate: moment(birthdate).format('MMMM DD'),
+                    });
+                });
+            }
+            if (newJoinees.length) {
+                newJoinees.forEach(({ name, email, work_info }) => {
+                    arrayNewJoinees.push({
+                        name: name,
+                        email: email,
+                        department: work_info.department,
+                        title: work_info.title,
+                        join_date: moment(work_info.join_date).format('MMMM DD, YYYY'),
+                    });
+                });
+            }
+            res.json({
+                isSuccess: true,
+                today: arrayToday,
+                missed: arrayMissed,
+                upcoming: arrayUpcoming,
+                referrals: arrayReferrals,
+                birthday_corner: arrayBirthdaycorner,
+                new_joinees: arrayNewJoinees,
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ error: err, message: 'Something Went Wrong' });
+    }
 };
