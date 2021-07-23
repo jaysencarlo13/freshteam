6f2f9408a621d37ebe08fcecc201be109c10fd9c
Jaysen <jaysencarlo13@gmail.com>
2021-07-23 20:08:25 +0800

    Add Jobpost and candidates to router

--- clientserver/src/App.js
+++ clientserver/src/App.js
@@ -6,36 +6,46 @@ import { Auth } from './Auth';
 import Logout from './components/logout/Logout';
 import ViewAccount from './components/view_account/ViewAccount';
 import Recruitment from './components/recruitment/Recruitment';
+import JobPost from './components/jobpost/JobPost';
+import Candidates from './components/recruitment/Candidates';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/register">
-          <Register />
-        </Route>
-        <Route exact path="/">
-          <Auth />
-          <Dashboard />
-        </Route>
-        <Route exact path="/viewaccount">
-          <Auth />
-          <ViewAccount />
-        </Route>
-        <Route exact path="/recruitment">
-          <Auth />
-          <Recruitment />
-        </Route>
-        <Route exact path="/logout">
-          <Logout />
-          <Auth />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/register">
+                    <Register />
+                </Route>
+                <Route exact path="/">
+                    <Auth />
+                    <Dashboard />
+                </Route>
+                <Route exact path="/viewaccount">
+                    <Auth />
+                    <ViewAccount />
+                </Route>
+                <Route exact path="/recruitment">
+                    <Auth />
+                    <Recruitment />
+                </Route>
+                <Route exact path="/jobpost/view">
+                    <Auth />
+                    <JobPost />
+                </Route>
+                <Route exact path="/recruitment/candidates">
+                    <Auth />
+                    <Candidates />
+                </Route>
+                <Route exact path="/logout">
+                    <Logout />
+                    <Auth />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
