2878517cc7f5973e30da376c1b04d61297682438
Jaysen <jaysencarlo13@gmail.com>
2021-07-23 20:25:35 +0800

    job_postings.js

--- models/job_postings.js
+++ models/job_postings.js
@@ -1,29 +1,30 @@
 const { Types, Schema, model } = require('mongoose');
 
 const job_postings_schema = new Schema(
-  {
-    organization_id: {
-      type: Types.ObjectId,
+    {
+        organization_id: {
+            type: Types.ObjectId,
+        },
+        created_by: {
+            type: Types.ObjectId,
+        },
+        title: {
+            type: String,
+        },
+        range: {
+            type: String,
+        },
+        type: {
+            type: String,
+        },
+        editor: {
+            type: String,
+        },
     },
-    created_by: {
-      type: Types.ObjectId,
-    },
-    title: {
-      type: String,
-    },
-    range: {
-      type: String,
-    },
-    type: {
-      type: String,
-    },
-    editor: {
-      type: String,
-    },
-  },
-  {
-    collection: 'job_postings',
-  }
+    {
+        collection: 'job_postings',
+        timestamps: true,
+    }
 );
 
 module.exports = model('job_postings_schema', job_postings_schema);
