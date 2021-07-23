abe344826b7a72bf7ca3ba6c4ebb77bfe2729062
Jaysen <jaysencarlo13@gmail.com>
2021-07-23 20:09:19 +0800

    White space only

--- clientserver/src/Auth.js
+++ clientserver/src/Auth.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Auth() {
-	const _data = JSON.parse(localStorage.getItem('data'));
-	const [data] = useState(_data);
-	if (data && data.user && data.sessionID) {
-		return '';
-	} else {
-		return <Redirect to="/login" />;
-	}
+    const _data = JSON.parse(localStorage.getItem('data'));
+    const [data] = useState(_data);
+    if (data && data.user && data.sessionID) {
+        return '';
+    } else {
+        return <Redirect to="/login" />;
+    }
 }
 
 function ServerAuth() {
-	localStorage.clear();
-	return <Redirect to="/login" />;
+    localStorage.clear();
+    return <Redirect to="/login" />;
 }
 
 export { Auth, ServerAuth };
